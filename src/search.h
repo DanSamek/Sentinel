3973b448e06e6b7bf3c9cdb0d3627d522b9ba2c0
Daniel Samek <samekda3@fit.cvut.cz>
2024-09-01 17:58:08 +0200

    another time management - Games: 1398, Wins: 455, Losses: 401, Draws: 542, Points: 726.0 (51.93 %)  [lazy to make a full SPRT]

--- src/search.h
+++ src/search.h
@@ -53,17 +53,17 @@ public:
         _bestScoreIter = INT_MIN;
         _bestMoveIter = {};
 
-        // minimum search time at least for depth = 1 - for some move.
         // TODO move this piece of code to timeManagement or something.
+        // minimum search time at least for depth = 1 - for some move.
         const auto minMs = 1;
 
-        auto msCanBeUsed = exact ? timeRemaining : timeRemaining / 25;
+        auto msCanBeUsed = exact ? timeRemaining : timeRemaining / 18;
         // increment
-        msCanBeUsed += increment / 2;
+        msCanBeUsed += increment / 2 + increment / 4;
 
         // if we are out of time, clamp it.
         if(msCanBeUsed >= timeRemaining && !exact){
-            msCanBeUsed = std::clamp(msCanBeUsed, minMs, timeRemaining / 25);
+            msCanBeUsed = std::clamp(msCanBeUsed, minMs, timeRemaining / 18);
         }
 
         _timer = Timer(msCanBeUsed);
