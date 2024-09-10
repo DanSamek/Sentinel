f29d0b418f653df565cf15829607a5188803dd60
Daniel Samek <samekda3@fit.cvut.cz>
2024-09-10 21:27:21 +0200

    timemanager changes

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -2,16 +2,15 @@
 #include "timemanager.h"
 
 int Timemanager::getSearchTime(int timeRemaining, int increment, bool exact) {
-    // minimum search time at least for depth = 1 - for some move.
-    const auto minMs = 1;
+    const auto minMs = 5;
 
-    auto msCanBeUsed = exact ? timeRemaining : timeRemaining / 18;
+    auto msCanBeUsed = exact ? timeRemaining : timeRemaining / 20;
     // increment
     msCanBeUsed += increment / 2 + increment / 4;
 
     // if we are out of time, clamp it.
     if(msCanBeUsed >= timeRemaining && !exact){
-        msCanBeUsed = std::clamp(msCanBeUsed, minMs, timeRemaining / 18);
+        msCanBeUsed = std::clamp(msCanBeUsed, minMs, timeRemaining / 20);
     }
     return msCanBeUsed;
 }
